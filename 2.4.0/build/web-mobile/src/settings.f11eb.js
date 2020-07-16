1ab8d31b111a65b62ebb34b1802a14c4da26cbbd
xuyanfeng <xu_yanfeng@126.com>
2020-07-16 09:58:30 +0800

    build

---
+++ 2.4.0/build/web-mobile/src/settings.f11eb.js
@@ -0,0 +1,119 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "Default",
+        "Background",
+        "Actor",
+        "Platform",
+        "Wall",
+        "Collider",
+        "Bullet",
+        "Ui",
+        "Ball"
+    ],
+    collisionMatrix: [
+        [
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false
+        ],
+        [
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false
+        ],
+        [
+            false,
+            false,
+            false,
+            true,
+            true,
+            false,
+            false
+        ],
+        [
+            false,
+            false,
+            true,
+            false,
+            false,
+            false,
+            true
+        ],
+        [
+            false,
+            false,
+            true,
+            false,
+            false,
+            false,
+            false,
+            false,
+            true
+        ],
+        [
+            false,
+            false,
+            false,
+            false,
+            false,
+            true,
+            false
+        ],
+        [
+            false,
+            false,
+            false,
+            true,
+            false,
+            false,
+            false
+        ],
+        [
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false
+        ],
+        [
+            false,
+            false,
+            false,
+            false,
+            true,
+            false,
+            false,
+            false,
+            true
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/TestList.fire",
+    orientation: "landscape",
+    debug: true,
+    jsList: [
+        "assets/cases/05_scripting/10_network/socket-io.b729e.js",
+        "assets/scripts/Init Engine.6e880.js"
+    ],
+    bundleVers: {
+        internal: "c9161",
+        TestBundle: "8c9f8",
+        resources: "0c748",
+        main: "83af3"
+    }
+};
