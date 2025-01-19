23d025044c6e10d2fd1db7f39a9ae72843c3bd7c
xu_yanfeng <xu_yanfeng@126.com>
2025-01-19 17:56:20 +0800

    add 2.4.13 debug版本

---
+++ 2.4.13/build/web-mobile-debug/src/settings.c1496.js
@@ -0,0 +1,119 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "Default",
+        "Background",
+        "Actor",
+        "Platform",
+        "Wall",
+        "Collider",
+        "Bullet",
+        "Ui",
+        "Ball"
+    ],
+    collisionMatrix: [
+        [
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false
+        ],
+        [
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false
+        ],
+        [
+            false,
+            false,
+            false,
+            true,
+            true,
+            false,
+            false
+        ],
+        [
+            false,
+            false,
+            true,
+            false,
+            false,
+            false,
+            true
+        ],
+        [
+            false,
+            false,
+            true,
+            false,
+            false,
+            false,
+            false,
+            false,
+            true
+        ],
+        [
+            false,
+            false,
+            false,
+            false,
+            false,
+            true,
+            false
+        ],
+        [
+            false,
+            false,
+            false,
+            true,
+            false,
+            false,
+            false
+        ],
+        [
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false
+        ],
+        [
+            false,
+            false,
+            false,
+            false,
+            true,
+            false,
+            false,
+            false,
+            true
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/TestList.fire",
+    orientation: "landscape",
+    debug: true,
+    jsList: [
+        "assets/cases/05_scripting/10_network/socket-io.66ad3.js",
+        "assets/scripts/Init Engine.0f770.js"
+    ],
+    bundleVers: {
+        internal: "cb4f5",
+        resources: "1af3f",
+        TestBundle: "9ec1e",
+        main: "c12af"
+    }
+};
