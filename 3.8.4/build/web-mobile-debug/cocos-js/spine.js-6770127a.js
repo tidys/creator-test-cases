f9e1d033962d11e94e64edf9a2b7d086d3fd710e
xu_yanfeng <xu_yanfeng@126.com>
2025-02-14 14:16:53 +0800

    3.8.4 debug

---
+++ 3.8.4/build/web-mobile-debug/cocos-js/spine.js-6770127a.js
@@ -0,0 +1,11 @@
+System.register([], (function (exports, module) {
+	'use strict';
+	return {
+		execute: (function () {
+
+			var spine_js = exports('default', 'assets/spine.js.mem-dc937d48.bin'); /* asset-hash:74cbe1d2 */
+
+		})
+	};
+}));
+//# sourceMappingURL=spine.js-6770127a.js.map
